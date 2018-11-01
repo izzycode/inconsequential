61739523f807908dd10d612eeb10dc644d50750c
William Kontos <williamjkontos@gmail.com>
2018-11-01 15:08:36 -0400

    added color coded due dates

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+  def badge_class_for(task)
+    if task.due_later?
+      "badge badge-success"
+    elsif task.due_soon?
+      "badge badge-warning"
+    elsif task.past_due?
+      "badge badge-danger"
+    else
+      ""
+    end
+  end
 end
