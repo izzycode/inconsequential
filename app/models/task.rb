9d03792d04818dece29db5adba5abf2292d03db4
Eduard Fernandez <efern218@fiu.edu>
2018-11-01 14:09:17 -0400

    Add Pending and Completed Nav

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :description, presence: true
   has_many :sub_tasks
+  scope :completed, -> { where(completed: true)}
+  scope :pending, -> { where(completed: false)}
 end
